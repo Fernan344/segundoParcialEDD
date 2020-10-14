d6dc5127f4a919860eac1a0c45d1620d14c6649b
Fernan344 <41765781+Fernan344@users.noreply.github.com>
2020-10-14 09:01:34 -0600

    pseudo

--- Problema7Examen.cpp
+++ Problema7Examen.cpp
@@ -10,8 +10,11 @@ int main()
 {
     Arbol* arbol = new Arbol();
     string entrada = "a a a a b b b b b ca ca ca ca de de de de de de de de de";
-
-
+    //separar por palabras
+    //hacer un ciclo de 0 hasta la cantidad de palabras
+    //llamar el metodo add del objeto arbol en cada vuelta del ciclo
+    //pasarle como parametro la palabra en la posicion de vuelta del ciclo
+    //al terminar el ciclo se invoca el metodo imprimir del objeto arbol
     std::cout << "Hello World!\n";
 }
 
